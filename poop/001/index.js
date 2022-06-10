de5d2dee3e816b286a51bc27d86428ea6e4b98e4
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-06-10 09:52:52 -0300

    playing with js and html

---
+++ poop/001/index.js
@@ -0,0 +1,7 @@
+const pessoa = {
+    nome: 'Luccas',
+    idade: 20,
+    peso: 74.8
+}
+
+document.write(`Dados da pessoa</br>Nome...: ${pessoa.nome}</br>Idade...: ${pessoa.idade}</br>Peso...: ${pessoa.peso}`)
